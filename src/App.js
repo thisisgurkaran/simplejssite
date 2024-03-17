c6fc98030102a2747a326a8f74e9561687bd932d
thisisgurkaran <gurkaran.12@gmail.com>
2024-03-16 22:22:12 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    const response = axios.get(
+      "https://7kzsh2patg.execute-api.us-east-1.amazonaws.com/default/github-to-lambda-demo/getproducts"
+    );
+    console.log(response);
+  }, []);
   return <div className="app">this is a simple react app</div>;
 }
 
